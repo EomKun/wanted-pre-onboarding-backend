f5f15724d975225e52a38e49dfe973406dbd3ed6
EomKun <zzang6535@gmail.com>
2023-08-16 18:00:03 +0900

    :bulb:[ADD][00003] 컨트롤러 명세 기입
    - 컨트롤러를 회원, 게시판으로 나누어 명세를 작성하였습니다.

---
+++ src/main/java/com/example/board/wantedpreonboardingbackend/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.board.wantedpreonboardingbackend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    /**
+     * 회원 관련된 요청을 받아 처리하는 부분
+     *
+     * 1. 사용자 회원가입 엔드포인트
+     * 2. 사용자 로그인 엔드포인트
+     *
+     * 2가지의 구현이 필요합니다.
+     */
+}
